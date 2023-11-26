c2ec8468c0795925fe195a39209b6ae96ddb5d70
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2023-11-26 19:40:49 +0300

    add progect

---
+++ src/hex2rgb/HEX2RGB.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+export default function HEX2RGB () {
+
+  const [hexValue, setHEX] = useState('');
+
+  const [rgb, setRGB] = useState({
+    rgbText: 'RGB',
+    rgbValue: '',
+    rgbaValue: ''
+  });
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+  }
+
+  const onChangeHandler = (e) => {
+
+    const hex = e.target.value;
+
+    if (hex.length > 7) {
+      return false;
+    }
+
+    setHEX(hex);
+
+    if (hex.length === 7) {
+      showRGB(hex);
+    }
+  }
+
+  const showRGB = (hex) => {
+    const newHexValue = hex.split('');
+
+    if (newHexValue[0] !== '#') {
+      error();
+      return false;
+    }
+
+    for (let i = 1; i < 7; i++) {
+      if (!/[0-9a-fA-F]+/.test(newHexValue[i])) {
+        error();
+        return false;
+      }
+    }
+
+    const firstNumRgb = hexToDec(newHexValue[1] + newHexValue[2]);
+    const secondNumRgb = hexToDec(newHexValue[3] + newHexValue[4]);
+    const thirdNumRgb = hexToDec(newHexValue[5] + newHexValue[6]);
+
+    setRGB({
+      rgbText: 'rgb(' + firstNumRgb + ', ' + secondNumRgb + ', ' + thirdNumRgb + ')',
+      rgbValue: 'rgb(' + firstNumRgb + ', ' + secondNumRgb + ', ' + thirdNumRgb + ')',
+      rgbaValue: 'rgba(' + firstNumRgb + ', ' + secondNumRgb + ', ' + thirdNumRgb + ', 0.5)'
+    });
+
+  }
+
+  const error = () => {
+    setRGB({
+      rgbText: 'Ошибка',
+      rgbValue: 'rgb(255, 0, 0)',
+      rgbaValue: 'rgba(255, 0, 0, 0.5)'
+    });    
+  }
+
+  const hexToDec = (hex) => {
+    return parseInt(hex, 16);
+  }
+
+  return (
+    <div className="wrapper" style={{backgroundColor: rgb.rgbaValue }}>
+      <div className="converter">
+        <form onSubmit={onSubmitForm}>
+          <input 
+            className="convert-input"
+            value={hexValue} placeholder="Цвет в формате #e6f0ff"
+            onChange={onChangeHandler} />
+        </form>
+        <div className="convert-result" style={{backgroundColor: rgb.rgbValue }}>
+          {rgb.rgbText}
+        </div>
+      </div>
+    </div>
+  )
+}
